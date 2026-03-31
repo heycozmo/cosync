ce38eaaaaf9ad01c17e0a5155d88436b76d3f6a7
heycozmo <itzcozmo@icloud.com>
2026-03-30 21:48:30 -0500

    testing git

--- integrations/discord_bot.py
+++ integrations/discord_bot.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+TOKEN = os.getenv("DISCORD_BOT_TOKEN") 
 
 intents = discord.Intents.default()
 intents.message_content = True
